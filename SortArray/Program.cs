b78cd7abfa1beb9fae0fd215d3c6ad3199574523
SaltykovAlexx <49457364+SaltykovAlexx@users.noreply.github.com>
2019-04-16 00:54:07 +0300

    First upload

---
+++ SortArray/Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = GetArray();
+            BubbleSort(myArray);
+            int compares = 0;
+            int swaps = 0;
+            int[] arrQuick = QuickSort(myArray, 0, myArray.Length - 1, ref compares, ref swaps);
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("QuickSort");
+            Console.WriteLine();
+            Console.WriteLine("Compares: {0}", compares);
+            Console.WriteLine("Swaps: {0}", swaps);
+            Console.WriteLine();
+            Console.WriteLine("Array (after sorting): ");
+            for (int i = 0; i < arrQuick.Length; i++)
+            {
+                Console.Write("{0}\t", arrQuick[i]);
+            }
+            Console.ReadKey();
+        }
+
+
+        public static int[] GetArray()
+        {
+            Random rnd = new Random();
+            int size = rnd.Next(5, 20);
+
+            int[] arr = new int[size];
+            Console.WriteLine("Array length: {0}", arr.Length);
+
+            Console.WriteLine("Array (before sorting): ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rnd.Next(-100, 100);
+                Console.Write("{0}\t", arr[i]);
+            }
+            return arr;
+        }
+
+        public static int[] BubbleSort(int[] arr)
+        {
+            int compares = 0;
+            int swaps = 0;
+            int i, j;
+            int[] arrBubble = arr;
+            for (i = 0; i < arrBubble.Length; i++)
+            {
+                for (j = 0; j < arrBubble.Length; j++)
+                {
+                    if (arrBubble[i] < arrBubble[j])
+                    {
+                        compares++;
+                        swaps++;
+                        Swap(arrBubble, i, j);
+                    }
+                    else
+                    {
+                        compares++;
+                    }
+                }
+                
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("BubbleSort");
+            Console.WriteLine();
+            Console.WriteLine("Compares: {0}", compares);
+            Console.WriteLine("Swaps: {0}", swaps);
+            Console.WriteLine();
+            Console.WriteLine("Array (after sorting): ");
+            for (i = 0; i < arrBubble.Length; i++)
+            {
+                Console.Write("{0}\t", arrBubble[i]);
+            }
+
+            return arrBubble;
+        }
+
+        public static void Swap(int[] arr, int i, int j)
+        {
+            int temp = arr[j];
+            arr[j] = arr[i];
+            arr[i] = temp;
+        }
+
+        public static int[] QuickSort(int[] arr, int low, int high, ref int compares, ref int swaps)
+        {
+            int[] arrQuick = arr;
+            int i, j;
+            if (low < high)
+            {
+                compares++;
+                int pivotIndex = (int)(high/2);
+                int newPivot = Partition(arrQuick, low, high, pivotIndex, ref compares, ref swaps);
+
+                QuickSort(arrQuick, low, newPivot - 1, ref compares, ref swaps);
+                QuickSort(arrQuick, newPivot + 1, high, ref compares, ref swaps);
+            }
+
+
+            return arrQuick;
+            
+        }
+
+        public static int Partition(int[] arr, int low, int high, int pivotIndex, ref int compares, ref int swaps)
+        {
+            int pivotValue = arr[pivotIndex];
+
+            Swap(arr, pivotIndex, high);
+            swaps++;
+
+            int storeIndex = low;
+
+            for (int i = low; i < high; i++)
+            {
+                if (arr[i].CompareTo(pivotValue) < 0)
+                {
+                    Swap(arr, i, storeIndex);
+                    storeIndex++;
+                    compares++;
+                    swaps++;
+                }
+                else
+                {
+                    compares++;
+                }
+            }
+
+            Swap(arr, storeIndex, high);
+            swaps++;
+            return storeIndex;
+        }
+
+
+
+
+
+
+    }
+}
---
+++ SortArray/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("SortArray")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SortArray")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("fa8986be-91d8-4d91-a71c-ba053f0f6e85")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
